61694060813ca02b69db5390fdd26fe700493d90
Jakarin-Jojo <jakarin.chujan@gmail.com>
2023-02-08 22:33:12 +0700

    Display selectedDate and Formatting

--- app/src/main/java/eu/tutorials/ageinminutes/MainActivity.kt
+++ app/src/main/java/eu/tutorials/ageinminutes/MainActivity.kt
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.text.SimpleDateFormat
 import java.time.Year
-import java.util.Calendar
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var tvSelectedDate : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
         btnDatePicker.setOnClickListener { clickDatePicker() }
     }
 
@@ -23,9 +27,16 @@ class MainActivity : AppCompatActivity() {
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
         DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener{view, year, month,dayOfMonth ->
+            DatePickerDialog.OnDateSetListener{view, selectedYear, selectedMonth, selectedDayOfMonth ->
                 Toast.makeText(this,
-                    "DatePicker Works",Toast.LENGTH_LONG).show()
+                    "Year was $selectedYear, month was ${selectedMonth+1}, day of month was $selectedDayOfMonth",Toast.LENGTH_LONG).show()
+
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+                tvSelectedDate?.text = selectedDate
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                val theDate = sdf.parse(selectedDate)
             },
             year,
             month,
